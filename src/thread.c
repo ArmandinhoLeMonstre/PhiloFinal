22bdcd098bf08aa65c02cb6d792a65c59aab9279
Armand Mititelu <armitite@fu-r8-p8.s19.be>
2025-01-01 21:59:53 +0100

    fct qui free

--- src/thread.c
+++ src/thread.c
@@ -6,7 +6,7 @@
 /*   By: armitite <armitite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 19:15:07 by armitite          #+#    #+#             */
-/*   Updated: 2025/01/01 21:24:22 by armitite         ###   ########.fr       */
+/*   Updated: 2025/01/01 21:57:11 by armitite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,30 +83,12 @@ int create_philo(t_philo **philo, t_data *data)
     while (i < data->p_total) 
     {
         if (pthread_join(th[i], NULL) != 0) 
-		{
             return 1;
-        }
         i++;
     }
     if (pthread_join(monitor, NULL) != 0) 
-    {
         return 1;
-    }
     i = 0;
-    while (i < data->p_total)
-    {
-        free(philo[i]);
-        i++;
-    }
-    i = 0;
-    free(philo);
-    while (i < data->p_total)
-    {
-        pthread_mutex_destroy(&data->mutex_forks[i]);
-        i++;
-    }
-    free(data->mutex_forks);
-    pthread_mutex_destroy(&data->mutex_print);
-	pthread_mutex_destroy(&data->mutex_data);
+	free_n_destroy(philo, data);
     return (0);
 }
